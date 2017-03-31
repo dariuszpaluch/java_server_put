56a9669ff6f58ca4562078d4aeaf6cbed3f225c8
Dariusz Paluch <dariusz.paluch@hotmail.com>
2017-03-31 11:16:21 +0200

    Add statistics to proxy

--- proxy-server/src/com/dariuszpaluch/TPSIServer.java
+++ proxy-server/src/com/dariuszpaluch/TPSIServer.java
@@ -16,27 +16,114 @@ import java.util.*;
 public class TPSIServer {
 
 	public static void main(String[] args) throws Exception {
-		int port = 8000;
+		int port = 8012;
 		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 		server.createContext("/", new RootHandler());
 		System.out.println("Starting server on port: " + port);
 		server.start();
 	}
 
+
+
 	static class RootHandler implements HttpHandler {
-		private final HashMap<String, Integer> statistics;
+
+		public class StatisticItem {
+			long dataSize;
+			String host;
+			int amount;
+
+			public int getAmount() {
+				return amount;
+			}
+
+			public long getDataSize() {
+				return dataSize;
+			}
+			public String getHost() {
+				return host;
+			}
+
+			StatisticItem(String host, int dataSize){
+				this.amount = 0;
+				this.dataSize = dataSize;
+				this.host = host;
+			}
+
+			public StatisticItem add(int dataSize) {
+				this.amount++;
+				this.dataSize+=dataSize;
+				return this;
+			}
+
+		}
+//		private final HashMap<String, StatisticItem> statistics;
 		private List<String> blackList;
+		private final  List<StatisticItem> statistics;
 		private boolean readedFile = false;
 
 		public RootHandler() {
 
-			this.statistics = new HashMap<>();
+			this.statistics = new ArrayList<>();
+
 			this.readBlackListFile();
 		}
 
-		public void addPathToStatistics(String path) {
-			statistics.merge(path, 1, (a, b) -> a + b); //add with 1, or  value++
+		public void saveStatisticsToCSV() {
+			PrintWriter pw = null;
+			try {
+				pw = new PrintWriter(new File("statistics.csv"));
+				StringBuilder sb = new StringBuilder();
+				sb.append("host");
+				sb.append(';-');
+				sb.append("amount");
+				sb.append(';');
+				sb.append("dataSize");
+				sb.append('\n');
+
+				for(StatisticItem statisticItem : this.statistics) {
+					sb.append(statisticItem.getHost());
+					sb.append(';');
+					sb.append(Integer.toString(statisticItem.getAmount()));
+					sb.append(';');
+					sb.append(Long.toString(statisticItem.getDataSize()));
+					sb.append('\n');
+				}
+
+
+				pw.write(sb.toString());
+				pw.close();
+				System.out.println("done!");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void addPathToStatistics(String host, int dataSize) {
+
+			boolean added = false;
+			for(StatisticItem item: this.statistics) {
+				if(item.getHost().equals(host)) {
+					item.add(dataSize);
+					added = true;
+				}
+			}
+
+			if(!added) {
+				this.statistics.add(new StatisticItem(host, dataSize));
+			}
+
+			this.saveStatisticsToCSV();
+
+//			i
+//			statistics.merge(host, new StatisticItem(host, dataSize), (a, b) -> a.add(dataSize)); //add with 1, or  value++
 //			System.out.println("Add to statistics: " + path);
+
+//			if(statistics.get(host) == null ){
+//				statistics.put(host, 1);
+//			}
+//				else {
+//			statistics.put(host, statistics.get(path) + 1);
+//			}
 		}
 
 		public static byte[] readFully(InputStream input) throws IOException
@@ -100,11 +187,6 @@ public class TPSIServer {
 				exchange.sendResponseHeaders(403, -1);
 			}
 			else {
-
-				//add statistics
-				this.addPathToStatistics(host);
-
-
 				//set request from client to server
 				URL url = new URL(path);
 				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -141,6 +223,7 @@ public class TPSIServer {
 				Map<String, List<String>> responseHeaders = connection.getHeaderFields();
 
 
+
 				//set response from server to client
 				responseHeaders.forEach((key, values) -> {
 					if(key != null && !Objects.equals(key, "Transfer-Encoding")) {
@@ -154,9 +237,14 @@ public class TPSIServer {
 				if(responseCode == HttpURLConnection.HTTP_NO_CONTENT
 						|| responseCode == HttpURLConnection.HTTP_NOT_MODIFIED) {
 					responseLength = -1;
+					//add statistics
+					this.addPathToStatistics(path, 0);
 				} else {
 					responseLength = responseBytes.length;
+					this.addPathToStatistics(path, responseLength);
 				}
+
+
 				OutputStream responseBody = exchange.getResponseBody();
 				exchange.sendResponseHeaders(responseCode, responseLength);
 				responseBody.write(responseBytes);
