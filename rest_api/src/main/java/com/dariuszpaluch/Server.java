f4e3a436105b92b9648d7766465ca9b1840d693e
Dariusz Paluch <dpaluch@psmm.pl>
2017-05-02 22:33:06 +0200

    Fix pom and compile to jar

---
+++ rest_api/src/main/java/com/dariuszpaluch/Server.java
@@ -0,0 +1,22 @@
+package com.dariuszpaluch;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.linking.DeclarativeLinkingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
+import java.net.URI;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        URI baseUri = UriBuilder.fromUri("http://0.0.0.0").port(9998).build();
+        ResourceConfig config = new ResourceConfig().packages("com.dariuszpaluch").registerClasses(
+                DeclarativeLinkingFeature.class
+        );
+
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
+        server.start();
+    }
+}
