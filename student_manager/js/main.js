8912b244082cf45d9f683e751bb9dee9382baf87
Dariusz Paluch <dariusz.paluch@hotmail.com>
2017-05-30 00:39:49 +0200

    Get first data from rest api

--- student_manager/js/main.js
+++ student_manager/js/main.js
@@ -1,18 +1,51 @@
 "use strict";
 
-{
-    var myObservableArray = ko.observableArray();    // Initially an empty array
-    $.ajax({
-        url:
-        contentType: 'application/json; charset=UTF-8',
-        dataType: "json",
+var API = "http://localhost:9999/";
 
-        // Instructions for how to deserialize a `mycustomtype`
-        converters: {
-            'text mycustomtype': function(result) {
-                // Do Stuff
-                return newresult;
-            }
-        },
+var prepareModel = function (query) {
+  var self = ko.observableArray();
+
+  const url = API + query;
+  self.url = url;
+
+  self.get = function () {
+    $.ajax({
+      type: 'GET',
+      url: url,
+      contentType: 'application/json; charset=UTF-8',
+      dataType: "json",
+      success: function (data) {
+        console.log('Pobrano: ' + url);
+        data.forEach(function (item) {
+          const object = ko.mapping.fromJS(item, { ignore: ["link"] });
+          self.push(object);
+        });
+      },
+      error: function (error) {
+
+      }
     });
+  };
+
+  self.get();
+
+  return self;
+};
+
+function Student(data) {
+  this.firstName = ko.observable(data.firstName);
+  this.lastName = ko.observable(data.lastName);
+
 }
+
+function studentManagerViewModel() {
+  var self = this;
+  self.students = prepareModel("students");
+  self.courses = prepareModel("courses");
+  self.grades = prepareModel("students/1/grades"); //TODO temporary
+}
+
+
+$(document).ready(function () {
+  ko.applyBindings(new studentManagerViewModel());
+});
